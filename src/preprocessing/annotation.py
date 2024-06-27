82a4290da920905c6f03c890b02cb2a2af9f5fa9
PrayonFernando <prayonfernando@gmail.com>
2024-06-27 11:02:21 +0530

    feature_extraction model

--- src/preprocessing/annotation.py
+++ src/preprocessing/annotation.py
@@ -1,48 +1,74 @@
 import json
 import os
+import re
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 
+# def annotate_data(data):
+#     """Annotates legal case data with BIO tags."""
+#     annotated_data = []
+#
+#     for case in data:
+#         # Process the 'name' field
+#         name_tokens = case["name"].split()
+#         name_labels = ["B-CASE_NAME"] + ["I-CASE_NAME"] * (len(name_tokens) - 1)
+#
+#         # Process the 'court_name' field
+#         court_tokens = case["court"]["name"].split()
+#         court_labels = ["B-COURT_NAME"] + ["I-COURT_NAME"] * (len(court_tokens) - 1)
+#
+#         # Process the 'decision_date' field
+#         date_tokens = case["decision_date"].split("-")
+#         date_labels = ["B-DATE"] + ["I-DATE"] * (len(date_tokens) - 1)
+#
+#         # Process the 'citations' field
+#         citation_tokens = []
+#         citation_labels = []
+#         for citation in case.get("citations", []):
+#             cite_text = citation["cite"]
+#             tokens = cite_text.split()
+#             labels = ["B-CITATION"] + ["I-CITATION"] * (len(tokens) - 1)
+#             citation_tokens.extend(tokens)
+#             citation_labels.extend(labels)
+#
+#         # Combine tokens and labels from all fields
+#         all_tokens = name_tokens + court_tokens + date_tokens + citation_tokens
+#         all_labels = name_labels + court_labels + date_labels + citation_labels
+#
+#         annotated_data.append({
+#             "tokens": all_tokens,
+#             "labels": all_labels
+#         })
+#
+#     return annotated_data
+
+
 def annotate_data(data):
-    """Annotates legal case data with BIO tags."""
+    """Annotates citations in legal case data with BIO tagging."""
     annotated_data = []
 
     for case in data:
-        # Process the 'name' field
-        name_tokens = case["name"].split()
-        name_labels = ["B-CASE_NAME"] + ["I-CASE_NAME"] * (len(name_tokens) - 1)
+        text = case["name"]
+        tokens = text.split()
+        labels = ["O"] * len(tokens)
 
-        # Process the 'court_name' field
-        court_tokens = case["court"]["name"].split()
-        court_labels = ["B-COURT_NAME"] + ["I-COURT_NAME"] * (len(court_tokens) - 1)
-
-        # Process the 'decision_date' field
-        date_tokens = case["decision_date"].split("-")
-        date_labels = ["B-DATE"] + ["I-DATE"] * (len(date_tokens) - 1)
-
-        # Process the 'citations' field
-        citation_tokens = []
-        citation_labels = []
-        for citation in case.get("citations", []):
+        for citation in case["citations"]:
             cite_text = citation["cite"]
-            tokens = cite_text.split()
-            labels = ["B-CITATION"] + ["I-CITATION"] * (len(tokens) - 1)
-            citation_tokens.extend(tokens)
-            citation_labels.extend(labels)
+            # Use regex to find all occurrences of the citation in the text
+            for match in re.finditer(re.escape(cite_text), text):
+                start, end = match.span()
+                cite_tokens = text[start:end].split()
+                for i, token in enumerate(cite_tokens):
+                    if i == 0:
+                        labels[tokens.index(token)] = "B-CITATION"
+                    else:
+                        labels[tokens.index(token)] = "I-CITATION"
 
-        # Combine tokens and labels from all fields
-        all_tokens = name_tokens + court_tokens + date_tokens + citation_tokens
-        all_labels = name_labels + court_labels + date_labels + citation_labels
-
-        annotated_data.append({
-            "tokens": all_tokens,
-            "labels": all_labels
-        })
+        annotated_data.append({"tokens": tokens, "labels": labels})
 
     return annotated_data
-
 
 # Specify the directory to save the annotated data
 save_directory = "../../data/processed/annotated_data"  # Create this directory if it doesn't exist
